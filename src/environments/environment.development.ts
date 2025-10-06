8efd5b37304836dbfb585150fb00873deef0e2bf
AmirbarrabajaLeon <amircasan@gmail.com>
2025-10-06 15:38:10 -0500

    feat: add environment configuration for development and production

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  baseApiBaseUrl: 'http://localhost:3000',
+  usersEndpoint: 'http://localhost:3000/users',
+  incidentsEndpoint: 'http://localhost:3000/incidents',
+  assignmentsEndpoint: 'http://localhost:3000/assignments',
+  notificationsEndpoint: 'http://localhost:3000/notifications',
+  analyticsEndpoint: 'http://localhost:3000/analytics',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  //Up to change it for the real backend URL later
+  baseApiBaseUrl: 'http://localhost:3000',
+  usersEndpoint: 'http://localhost:3000/users',
+  incidentsEndpoint: 'http://localhost:3000/incidents',
+  assignmentsEndpoint: 'http://localhost:3000/assignments',
+  notificationsEndpoint: 'http://localhost:3000/notifications',
+  analyticsEndpoint: 'http://localhost:3000/analytics',
+};
