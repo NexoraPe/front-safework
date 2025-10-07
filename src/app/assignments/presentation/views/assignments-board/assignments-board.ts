57871a6e01c57c7fe4b4c2efe50ffb506de3cc0b
AmirbarrabajaLeon <amircasan@gmail.com>
2025-10-07 11:25:48 -0500

    feat(bounded-contexts): added pending bounded context and its structure
    - Just for the folder structure

---
+++ src/app/assignments/presentation/views/assignments-board/assignments-board.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assignments-board',
+  imports: [],
+  templateUrl: './assignments-board.html',
+  styleUrl: './assignments-board.css'
+})
+export class AssignmentsBoard {
+
+}
---
+++ src/app/incidents/presentations/views/incidents-list/incidents-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incidents-list',
+  imports: [],
+  templateUrl: './incidents-list.html',
+  styleUrl: './incidents-list.css'
+})
+export class IncidentsList {
+
+}
