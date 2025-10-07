a9eaf3baca1f5fcc8d3142a49b7a28bb6248cadd
AmirbarrabajaLeon <amircasan@gmail.com>
2025-10-07 17:09:41 -0500

    feat(layout): implement side navigation with logo and language switcher

--- src/app/shared/presentation/components/main-layout/main-layout.ts
+++ src/app/shared/presentation/components/main-layout/main-layout.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {Toolbar} from '../toolbar/toolbar';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
+import {NgOptimizedImage} from '@angular/common';
+import {LanguageSwitcher} from '../language-switcher/language-switcher';
+
 
 @Component({
   selector: 'app-main-layout',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    Toolbar,
+    MatSidenav,
+    MatSidenavContent,
+    MatSidenavContainer,
+    NgOptimizedImage,
+    LanguageSwitcher,
   ],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css'
 })
 export class MainLayout {
 
+  sidebarOptions = [
+    { link: '/app/profile', label: 'option.profile' },
+    { link: '/app/incidents', label: 'option.incidents' },
+    { link: '/app/assignments', label: 'option.assignments' },
+    { link: '/app/notifications', label: 'option.notifications' },
+    { link: '/app/analytics', label: 'option.analytics' }
+  ];
 }
