dc4d5dba0b7309783f623dfe72aea14c4e66cf48
AmirbarrabajaLeon <amircasan@gmail.com>
2025-10-07 17:09:35 -0500

    feat(toolbar): implement dynamic navigation with active link styling and translation support

--- src/app/shared/presentation/components/toolbar/toolbar.ts
+++ src/app/shared/presentation/components/toolbar/toolbar.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {MatListItem, MatNavList} from '@angular/material/list';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+import {TranslatePipe} from '@ngx-translate/core';
+import {MatButton} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [],
+  imports: [
+    MatNavList,
+    MatListItem,
+    RouterLink,
+    RouterLinkActive,
+    TranslatePipe,
+    MatListModule
+  ],
   templateUrl: './toolbar.html',
   styleUrl: './toolbar.css'
 })
 export class Toolbar {
-
+  @Input() options: any[] = [];
 }
