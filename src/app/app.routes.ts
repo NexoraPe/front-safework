7023b9098d084bfa1f2ee5b8613ff1317bbbdc39
AmirbarrabajaLeon <amircasan@gmail.com>
2025-10-06 20:40:15 -0500

    feat(routes): configure routing for IAM layout with login and registration components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginForm } from './IAM/presentation/views/login-form/login-form';
+import { RegisterForm } from './IAM/presentation/views/register-form/register-form';
+import { IamLayout } from './IAM/presentation/components/iam-layout/iam-layout';
+
+
+const baseTitle = 'Safework';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: IamLayout,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginForm, title: `${baseTitle} - Home` },
+      { path: 'register', component: RegisterForm, title: `${baseTitle} - Register` }
+    ]
+  },
+];
