2b1a7af74b1fdd956ea3dd0be66cf1ab86cc6e8c
AmirbarrabajaLeon <amircasan@gmail.com>
2025-10-07 17:08:18 -0500

    feat(routes): add main layout and new routes for profile, incidents, notifications, assignments, and analytics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,13 @@ import { LoginForm } from './IAM/presentation/views/login-form/login-form';
 import { RegisterForm } from './IAM/presentation/views/register-form/register-form';
 import { IamLayout } from './IAM/presentation/components/iam-layout/iam-layout';
 
+import { MainLayout} from './shared/presentation/components/main-layout/main-layout';
+
+import { ProfileForm } from './IAM/presentation/views/profile-form/profile-form';
+import { IncidentsList  } from './incidents/presentations/views/incidents-list/incidents-list';
+import { Notifications  } from './notifications/presentation/views/notifications/notifications';
+import { AssignmentsBoard  } from './assignments/presentation/views/assignments-board/assignments-board';
+import { Analytics  } from './analytics/presentation/views/analytics/analytics';
 
 const baseTitle = 'Safework';
 
@@ -17,4 +24,16 @@ export const routes: Routes = [
       { path: 'register', component: RegisterForm, title: `${baseTitle} - Register` }
     ]
   },
+  {
+    path: 'app',
+    component: MainLayout,
+    children: [
+      { path: '', redirectTo: 'profile', pathMatch: 'full' },
+      { path: 'profile', component: ProfileForm, title: `${baseTitle} - Profile` },
+      { path: 'incidents', component: IncidentsList, title: `${baseTitle} - Incidents` },
+      { path: 'notifications', component: Notifications, title: `${baseTitle} - Notifications` },
+      { path: 'assignments', component: AssignmentsBoard, title: `${baseTitle} - Assignments` },
+      { path: 'analytics', component: Analytics, title: `${baseTitle} - Analytics` }
+    ]
+  }
 ];
