1816d412f14b463e2d6c94c869bfd788608f19d0
AmirbarrabajaLeon <amircasan@gmail.com>
2025-10-09 03:40:48 -0500

    feat(environment): update API endpoints to point to production server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   //Up to change it for the real backend URL later or a deployed db.json server
-  baseApiBaseUrl: 'http://localhost:3000',
-  usersEndpoint: 'http://localhost:3000/users',
-  incidentsEndpoint: 'http://localhost:3000/incidents',
-  assignmentsEndpoint: 'http://localhost:3000/assignments',
-  notificationsEndpoint: 'http://localhost:3000/notifications',
-  analyticsEndpoint: 'http://localhost:3000/analytics',
+  baseApiBaseUrl: 'https://safeworkapi.azurewebsites.net',
+  usersEndpoint: 'https://safeworkapi.azurewebsites.net/users',
+  incidentsEndpoint: 'https://safeworkapi.azurewebsites.net/incidents',
+  assignmentsEndpoint: 'https://safeworkapi.azurewebsites.net/assignments',
+  notificationsEndpoint: 'https://safeworkapi.azurewebsites.net/notifications',
+  analyticsEndpoint: 'https://safeworkapi.azurewebsites.net/analytics',
 
   userEndpointPath: '/users',
   incidentEndpointPath: '/incidents',
